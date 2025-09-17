7bea562b36b7841a856a1404fa8324a909cf9d0b
Rajni Meghwal <meghwalrajni27@gmail.com>
2025-09-17 20:58:13 +0530

    Initial commit with main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+# print("hello python ") 
+
+# a = 'hello'
+# print(a[0:4:2])
+
+   
+# b = "vs code"
+
+
+
+#question1 
+number = int(input("enter your age :"))
+print(f"your age is {number}")
+
+
+#compound assignment operator
+a = 10
+a += 5
+print(a)
+
+# print(ord("R"))
+# print(ord("T"))
+
+print ("R" > "T")  
+# cannot compare string with an integer 
